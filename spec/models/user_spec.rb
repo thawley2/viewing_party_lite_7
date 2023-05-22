1c488d08d318419f5eb6179180aeecd31d948944
Thomas Hawley <thawley22@turing.edu>
2023-05-22 16:19:37 -0600

    test: Add test to validate presence of password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,7 +26,7 @@ RSpec.describe User, type: :model do
     it {should validate_presence_of(:email)}
     it {should validate_uniqueness_of(:email).case_insensitive}
     it {should validate_confirmation_of(:password)}
-    it {should validate_presence_of(:password_confirmation)}
+    it {should validate_presence_of(:password)}
     it {should have_secure_password}
   end
 
