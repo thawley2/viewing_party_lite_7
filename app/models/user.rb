8eccb03a0de7f352e9e64396c38e86ccbffe4fbf
Thomas Hawley <thawley22@turing.edu>
2023-05-22 12:59:19 -0600

    feat: Add validations for case insensitive email and password confirmation and presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   has_many :parties, through: :user_parties
 
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
 
   def hosted_parties
     parties.where("user_parties.is_host = true")
