1661b331612618fa9070811b8fcd52cb97379000
Thomas Hawley <thawley22@turing.edu>
2023-05-22 14:02:54 -0600

    test: Add sad path for all missing/not_unique data entered on form with specific errors

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -46,27 +46,61 @@ RSpec.describe '/register', type: :feature do
 
       fill_in 'Name', with: "#{user.name}"
       fill_in 'E-mail Address:', with: "#{user.email}"
+      fill_in 'Password:', with: '12345'
+      fill_in 'Confirm Password:', with: '12345'
       click_button 'Register'
 
-      expect(page).to have_content("A name and unique email must be present.")
+      expect(page).to have_content("Validation failed: Email has already been taken")
     end
 
     it 'should not allow users to register without a name' do
       user = create(:user)
 
-      fill_in 'E-mail Address:', with: "#{user.email}"
+      fill_in 'E-mail Address:', with: "ttt@gmail.com"
+      fill_in 'Password:', with: '12345'
+      fill_in 'Confirm Password:', with: '12345'
       click_button 'Register'
 
-      expect(page).to have_content("A name and unique email must be present.")
+      expect(page).to have_content("Validation failed: Name can't be blank")
     end
 
     it 'should not allow users to register without an e-mail address' do
       user = create(:user)
 
       fill_in 'Name', with: "#{user.name}"
+      fill_in 'Password:', with: '12345'
+      fill_in 'Confirm Password:', with: '12345'
       click_button 'Register'
 
-      expect(page).to have_content("A name and unique email must be present.")
+      expect(page).to have_content("Validation failed: Email can't be blank")
+    end
+
+    it 'should not allow users to register without filling in the password field' do
+      fill_in 'Name', with: 'Jane Doe'
+      fill_in 'E-mail Address:', with: 'jane-doe@gmail.com'
+      fill_in 'Confirm Password:', with: '12345'
+      click_button 'Register'
+
+      expect(page).to have_content("Validation failed: Password confirmation doesn't match Password, Password can't be blank")
+    end
+
+    it 'should not allow users to register without filling in the confirm password field' do
+      fill_in 'Name', with: 'Jane Doe'
+      fill_in 'E-mail Address:', with: 'jane-doe@gmail.com'
+      fill_in 'Password:', with: '12345'
+      click_button 'Register'
+
+      expect(page).to have_content("Password confirmation doesn't match Password, Password confirmation can't be blank")
+    end
+
+    it 'should not allow users to register if the password and confirm passwords dont match' do 
+      fill_in 'Name', with: 'Jane Doe'
+      fill_in 'E-mail Address:', with: 'jane-doe@gmail.com'
+      fill_in 'Password:', with: '12345'
+      fill_in 'Confirm Password:', with: '23456'
+      click_button 'Register'
+
+      expect(page).to have_content("Password confirmation doesn't match Password")
     end
   end
 end
