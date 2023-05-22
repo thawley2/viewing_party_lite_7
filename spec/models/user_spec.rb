867c82a1670c2d98255ea457d08c0373eabd3b9f
Thomas Hawley <thawley22@turing.edu>
2023-05-22 13:43:01 -0600

    test: Add test for attribute password and password_digest is working correctly

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,6 +27,16 @@ RSpec.describe User, type: :model do
     it {should validate_uniqueness_of(:email).case_insensitive}
     it {should validate_confirmation_of(:password)}
     it {should validate_presence_of(:password_confirmation)}
+    it {should have_secure_password}
+  end
+
+  describe 'attributes' do
+    it 'user has password_digest attribute' do
+      user = User.create(name: 'Meg', email: 'meg@test.com', password: 'password123', password_confirmation: 'password123')
+  
+      expect(user).to_not have_attribute(:password)
+      expect(user.password_digest).to_not eq('password123')
+    end
   end
   
   describe 'instance methods' do
