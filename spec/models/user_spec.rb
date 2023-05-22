551c3af5cd3b379ba7f46f885cf26040cdbd9fe3
Thomas Hawley <thawley22@turing.edu>
2023-05-22 12:58:46 -0600

    test: Add tests to validate password and update email for case insensitive

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,7 +24,9 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it {should validate_presence_of(:name)}
     it {should validate_presence_of(:email)}
-    it {should validate_uniqueness_of(:email)}
+    it {should validate_uniqueness_of(:email).case_insensitive}
+    it {should validate_confirmation_of(:password)}
+    it {should validate_presence_of(:password_confirmation)}
   end
 
   describe 'instance methods' do
