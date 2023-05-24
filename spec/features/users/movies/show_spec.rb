7aebb08a556e66b51aef1eb109eed3d62e4fe36e
Thomas Hawley <thawley22@turing.edu>
2023-05-23 19:12:09 -0600

    test: Add sad path test for clicking create party while not logged in

--- spec/features/users/movies/show_spec.rb
+++ spec/features/users/movies/show_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe '/users/:id/movies/:id' do
   before(:each) do
     VCR.use_cassette('test_for_links_top_movies', :allow_playback_repeats => true) do
       @user1 = create(:user)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user1)
 
       visit discover_path
 
@@ -28,6 +27,8 @@ RSpec.describe '/users/:id/movies/:id' do
   describe 'When I visit a movies details page' do
     it 'should have a button that links to a page to create a new viewing party' do
       VCR.use_cassette('movie_details', :allow_playback_repeats => true) do
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user1)
+
         expect(page).to have_button('Create Viewing Party')
         
         click_button 'Create Viewing Party'
@@ -36,42 +37,51 @@ RSpec.describe '/users/:id/movies/:id' do
       end
     end
 
-      it 'should have a button that links to the users discover page' do
-        VCR.use_cassette('movie_details', :allow_playback_repeats => true) do
-          expect(page).to have_button('Discover Page')
-          
-          click_button 'Discover Page'
-          
-          expect(current_path).to eq(discover_path)
-        end
+    it 'should have a button that links to the discover page' do
+      VCR.use_cassette('movie_details', :allow_playback_repeats => true) do
+        expect(page).to have_button('Discover Page')
+        
+        click_button 'Discover Page'
+        
+        expect(current_path).to eq(discover_path)
       end
+    end
 
-      it 'should have information about the specific movie' do
-        VCR.use_cassette('movie_details', :allow_playback_repeats => true) do
-          expect(page).to have_content(@selected_movie.title)
-          expect(page).to have_content("Vote Average: #{@selected_movie.vote_average}")
-          expect(page).to have_content("Runtime: 2h 55m")
-          
-          @selected_movie.genres.each do |genre|
-            expect(page).to have_content(genre)
-          end
-          
-          expect(page).to have_content(@selected_movie.summary)
-          
-          expect(page).to have_css('.cast', maximum: 10)
-          
-          @cast.each do |cast_member|
-            expect(page).to have_content(cast_member.character)
-            expect(page).to have_content(cast_member.name)
-          end
-          
-          expect(page).to have_content("#{@reviews.count} Reviews")
-          
-          @reviews.each do |review|
-            expect(page).to have_content(review.author)
-            # expect(page).to have_content(review.content)
-          end
+    it 'should have information about the specific movie' do
+      VCR.use_cassette('movie_details', :allow_playback_repeats => true) do
+        expect(page).to have_content(@selected_movie.title)
+        expect(page).to have_content("Vote Average: #{@selected_movie.vote_average}")
+        expect(page).to have_content("Runtime: 2h 55m")
+        
+        @selected_movie.genres.each do |genre|
+          expect(page).to have_content(genre)
+        end
+        
+        expect(page).to have_content(@selected_movie.summary)
+        
+        expect(page).to have_css('.cast', maximum: 10)
+        
+        @cast.each do |cast_member|
+          expect(page).to have_content(cast_member.character)
+          expect(page).to have_content(cast_member.name)
+        end
+        
+        expect(page).to have_content("#{@reviews.count} Reviews")
+        
+        @reviews.each do |review|
+          expect(page).to have_content(review.author)
+          # expect(page).to have_content(review.content)
         end
       end
+    end
+
+    it 'returns an error message if a visitor is not logged in and clicks create viewing party' do
+      expect(page).to have_button('Create Viewing Party')
+        
+        click_button 'Create Viewing Party'
+        
+        expect(current_path).to eq(movie_path(@movie.id))
+        expect(page).to have_content('Must be logged in to create a Viewing Party.')
+    end
   end
 end
