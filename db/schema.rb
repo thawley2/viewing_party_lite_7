53b58781daad97363a62d9f7933096a7f693eaca
Thomas Hawley <thawley22@turing.edu>
2023-05-24 12:40:55 -0600

    Add migration to add role column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_22_185635) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_24_183945) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_22_185635) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.integer "role", default: 0
   end
 
   add_foreign_key "user_parties", "parties"
